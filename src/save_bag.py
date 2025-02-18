44411a4a9a68857870686a29d5191345aec5b604
PaoloFori <forin.paolo98@gmail.com>
2025-02-18 18:57:05 +0100

    adding two more topic to save

--- src/save_bag.py
+++ src/save_bag.py
@@ -16,7 +16,7 @@ class save_bag:
         
         bag_file = filepath + '/' + subject + '_' + date_string + '.bag'
         
-        topics = "/cvsa/eye /events/bus /cvsa/trials_keep /imu_cvsa"
+        topics = "/cvsa/eye /events/bus /cvsa/trials_keep /imu_cvsa /cvsa/neuroprediction /cvsa/features /cvsa/neuroprediction/integrated"
         record_command = f'rosbag record -O {bag_file} {topics}'
         
         # to save parameters used
